362b882902b86367a84b1a0c753293e887e8bfa7
Keith Ricker <keithricker@hotmail.com>
2015-07-28 23:57:11 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,24 @@ set -e
 
 [ ! -f /data/btsync.conf ] && cat > /data/btsync.conf <<EOF
 {
+  "device_name": "Sync Server",
   "listening_port": 3369,
+  "storage_path": "/.sync",
   "check_for_updates": false,
   "use_upnp": false,
+  "download_limit": 0,
+  "upload_limit": 0,
+  "shared_folders": [
+    {
+      "secret": "$BTSSECRET",
+      "dir": "/Sync",
+      "use_relay_server": true,
+      "use_tracker": true,
+      "use_dht": false,
+      "search_lan": true,
+      "use_sync_trash": false
+    }
+  ],  
   "webui": {
     "listen": "0.0.0.0:8888",
     "login": "${BTSUNAME}",
