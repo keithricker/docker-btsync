9a611b746258003c5d6835769df7a296d7e5abc9
Keith Ricker <keithricker@hotmail.com>
2015-08-09 07:35:31 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ ! -d "$SYNCDIR" ]; then mkdir "$SYNCDIR" && chmod 777 "$SYNCDIR"; fi
 
 [ ! -L /.sync ] && ln -sf /btsync /.sync
 
-[ ! -f /btsync/btsync.conf ] && cat > /btsync/btsync.conf <<EOF
+[ ! -f /btsync/btsync.conf && "$ENABLE_GUI" = 'noway' ] && cat > /btsync/btsync.conf <<EOF
 {
   "device_name": "Sync Server",
   "listening_port": 3369,
@@ -26,6 +26,22 @@ if [ ! -d "$SYNCDIR" ]; then mkdir "$SYNCDIR" && chmod 777 "$SYNCDIR"; fi
       "use_sync_trash": false
     }
   ],  
+  "webui": {
+    "listen": "0.0.0.0:8384",
+    "login": "${BTSUNAME}",
+    "password": "${BTSPASS}"
+  }
+}
+EOF
+
+[ ! -f /btsync/btsync.conf && "$ENABLE_GUI" = 'way' ] && cat > /btsync/btsync.conf <<EOF
+{
+  "device_name": "Sync Server",
+  "listening_port": 3369,
+  "check_for_updates": false,
+  "use_upnp": false,
+  "download_limit": 0,
+  "upload_limit": 0,
   "webui": {
     "listen": "0.0.0.0:8384",
     "login": "${BTSUNAME}",
